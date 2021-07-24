626efee73f54a453802bdd7bfbd8afbbefc5ea7d
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-07-24 11:53:48 -0300

    Implementando a Paginação no endpoint Categorias

--- src/main/java/com/rodrigodojo/cursomc/CursomcApplication.java
+++ src/main/java/com/rodrigodojo/cursomc/CursomcApplication.java
@@ -62,6 +62,11 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria c1 = new Categoria(null , "Informatica");
 		Categoria c2 = new Categoria(null , "Escritorio");
+		Categoria c3 = new Categoria(null , "Cama,mesa e banho");
+		Categoria c4 = new Categoria(null , "Eletrônico");
+		Categoria c5 = new Categoria(null , "Jardinagem");
+		Categoria c6 = new Categoria(null , "Decoração");
+		Categoria c7 = new Categoria(null , "Perfumaria");
 		
 		Produto p1 = new Produto(null,"Computador",2000.00);
 		Produto p2 = new Produto(null,"impressora",800.00);
@@ -75,7 +80,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p3.getCategorias().addAll(Arrays.asList(c1));
 		
 		
-		categoriaRepository.saveAll(Arrays.asList(c1,c2));
+		categoriaRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5,c6,c7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		
