47ffbf87745cccf7436986e10379f0b0f107cc19
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-07-24 12:50:54 -0300

    Começando o tratamento com a @anotation Validation

--- src/main/java/com/rodrigodojo/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigodojo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigodojo.cursomc.domain.Categoria;
+import com.rodrigodojo.cursomc.dto.CategoriaDTO;
 import com.rodrigodojo.cursomc.repositories.CategoriaRepository;
 import com.rodrigodojo.cursomc.services.exceptions.DataIntegrityException;
 import com.rodrigodojo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,5 +58,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String orderBy,String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction) , orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());	
 	}
 }
