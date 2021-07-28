9930c22e61eff1f8b0c8bc7e3c701ac370f179d7
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-07-28 15:34:46 -0300

    Melhorando as validaçoes personalizada para o endpoint

--- src/main/java/com/rodrigodojo/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/rodrigodojo/cursomc/resources/exception/ValidationError.java
@@ -7,17 +7,17 @@ public class ValidationError extends StandardError {
 
 	private static final long serialVersionUID = 1L;
 	
-	private List<FieldMessage> list = new ArrayList<>();
+	private List<FieldMessage> errors = new ArrayList<>();
 	
 	public ValidationError(Integer status, String msg, Long timeStamp) {
 		super(status, msg, timeStamp);
 	}
 
-	public List<FieldMessage> getList() {
-		return list;
+	public List<FieldMessage> getErrors() {
+		return errors;
 	}
 
 	public void addError(String fieldName , String messagem) {
-		list.add(new FieldMessage(fieldName,messagem));
+		errors.add(new FieldMessage(fieldName,messagem));
 	}
 }
