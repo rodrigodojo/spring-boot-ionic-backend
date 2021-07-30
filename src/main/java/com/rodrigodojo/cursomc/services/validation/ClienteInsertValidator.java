d0684ec58e23dc0b03223a870537cde5fec20ad3
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-07-30 12:56:50 -0300

    Adicionando uma annotation personalizada para CPFouCNPJ

---
+++ src/main/java/com/rodrigodojo/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,29 @@
+package com.rodrigodojo.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.rodrigodojo.cursomc.dto.ClienteNewDTO;
+import com.rodrigodojo.cursomc.resources.exception.FieldMessage;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
