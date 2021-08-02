d4671aa730ac17ea1ccbe5bea3050474796c4d2f
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-08-02 12:36:02 -0300

    Aprendendo a mexer com parametros vindo de um uri

---
+++ src/main/java/com/rodrigodojo/cursomc/dto/ProdutoDTO.java
@@ -0,0 +1,48 @@
+package com.rodrigodojo.cursomc.dto;
+
+import java.io.Serializable;
+
+public class ProdutoDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Integer id;
+	private String nome;
+	private Double preco;
+	
+	public ProdutoDTO() {}
+	
+	public ProdutoDTO(Integer id, String nome, Double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+
+
+}
