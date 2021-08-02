be36565930c97a0bbf2a0ba6d07c1fc1d782d6b3
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-08-02 12:18:06 -0300

    Criando o search para buscas de produtos

--- src/main/java/com/rodrigodojo/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/rodrigodojo/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,17 @@
 package com.rodrigodojo.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rodrigodojo.cursomc.domain.Produto;
+import com.rodrigodojo.cursomc.domain.Categoria;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	Page<Produto> search(String nome ,List<Categoria> categorias , Pageable pageRequest);
 }
